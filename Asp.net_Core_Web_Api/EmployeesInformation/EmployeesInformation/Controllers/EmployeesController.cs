1addd51b48794e3bd86b75b80bb98b3b4672908b
unknown <karpagam@MUMBAI-AD.PLURALSIGHT.COM>
2024-07-30 12:16:40 +0530

    api day 1 done

---
+++ Asp.net_Core_Web_Api/EmployeesInformation/EmployeesInformation/Controllers/EmployeesController.cs
@@ -0,0 +1,72 @@
+﻿using EmployeesInformation.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+namespace EmployeesInformation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetEmployees()
+        {
+            List<Employee> employees = EmployeesDataStore.GetEmployees();
+            return Ok(employees);
+        }
+
+       
+        [HttpGet("{id}")]
+        public IActionResult GetEmployee(int id)
+        {
+
+            Employee emp = EmployeesDataStore.GetEmployeeById(id);
+            if (emp == null)
+            {
+                return NotFound(); // Return 404 if the employee is not found
+            }
+            return Ok(emp);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteEmployee(int id)
+        {
+
+           bool result = EmployeesDataStore.DeleteEmployeeById(id);
+            if (result)
+            {
+                return Ok(); 
+            }
+            return StatusCode(500, "Internal server error");
+        }
+
+        [HttpPost]
+        public IActionResult PostEmployee([FromBody] Employee employee)
+        {
+            if (employee == null)
+            {
+                return BadRequest("Employee data is null.");
+            }
+
+            // Perform validation on the employee data
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                // Insert employee into the database
+                EmployeesDataStore.InsertEmployee( employee.Name, employee.JobTitle, employee.Salary);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and return an internal server error
+                Console.WriteLine($"An error occurred while inserting employee: {ex.Message}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+    }
+}
---
+++ Asp.net_Core_Web_Api/EmployeesInformation/EmployeesInformation/EmployeesDataStore.cs
@@ -0,0 +1,144 @@
+﻿using EmployeesInformation.Models;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace EmployeesInformation
+{
+    public class EmployeesDataStore
+    {
+      public static  List<Employee> Employees { get; set; }
+
+        static string connectionString = "Server=WSAMZN-8P0C1RTJ;Initial Catalog=EmployeesDatabase;User ID=sa;Password=Password@123;";
+       
+        public static List<Employee> GetEmployees()
+        {
+             List<Employee> Employees = new List<Employee>();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "select * from Employee";
+                SqlCommand command = new SqlCommand(query, connection);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            Employee emp = new Employee
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                JobTitle = reader.GetString(2),
+                                Salary = reader.GetDecimal(3)
+                            };
+                            Employees.Add(emp);
+                        }
+                        catch (Exception ex)
+                        {
+                            // Log or handle individual record conversion errors if needed
+                            Console.WriteLine($"Error reading employee record: {ex.Message}");
+                        }
+                    }
+                }
+                //connection.Close();
+            }
+           return Employees;
+           
+        }
+        public static Employee GetEmployeeById(int id)
+        {
+            Employee employee = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "select * from Employee where Id= @Id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        try
+                        {
+                            employee = new Employee
+                            {
+                                Id = reader.GetInt32(0),
+                                Name = reader.GetString(1),
+                                JobTitle = reader.GetString(2),
+                                Salary = reader.GetDecimal(3)
+                            };
+                          
+                        }
+                        catch (Exception ex)
+                        {
+                            // Log or handle individual record conversion errors if needed
+                            Console.WriteLine($"Error reading employee record: {ex.Message}");
+                        }
+                    }
+                }
+                //connection.Close();
+            }
+            return employee;
+
+        }
+
+        public static bool DeleteEmployeeById(int id)
+        {
+           
+            bool result = false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = " Delete from Employee where Id= @Id";
+                SqlCommand command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Id", id);
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    result = true;
+                }
+                else
+                {
+                    return result;
+                }
+
+            }
+            return result;
+
+        }
+
+        public static bool InsertEmployee(string name,string jobTitle,decimal salary)
+        {
+
+            bool result = false;
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "INSERT INTO Employee ( Name, JobTitle, Salary) VALUES (@Name, @JobTitle, @Salary)";
+                
+            SqlCommand command = new SqlCommand(query, connection);
+                
+                command.Parameters.AddWithValue("@Name", name);
+                command.Parameters.AddWithValue("@JobTitle", jobTitle);
+                command.Parameters.AddWithValue("@Salary", salary);
+
+                int rowsAffected = command.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    result = true;
+                }
+                else
+                {
+                    return result;
+                }
+
+            }
+            return result;
+
+        }
+    }
+}
